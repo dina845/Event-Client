7fc12f9c2801a876972934d73c06fd26695d673f
ester levin <e0527655638@gmail.com>
2019-08-26 11:21:23 +0300

    upload img

--- ClientSide/src/app/app-routing.module.ts
+++ ClientSide/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UploadComponent } from './gallery/images/upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'file',component:UploadComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ClientSide/src/app/gallery/images/upload/upload.component.ts
+++ ClientSide/src/app/gallery/images/upload/upload.component.ts
@@ -11,5 +11,21 @@ export class UploadComponent implements OnInit {
 
   ngOnInit() {
   }
+  name = 'Angular 4';
+  urls = [];
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+        var filesAmount = event.target.files.length;
+        for (let i = 0; i < filesAmount; i++) {
+                var reader = new FileReader();
 
+                reader.onload = (event:any) => {
+                  console.log(event.target.result);
+                   this.urls.push(event.target.result); 
+                }
+
+                reader.readAsDataURL(event.target.files[i]);
+        }
+    }
+  }
 }
