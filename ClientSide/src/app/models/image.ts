57ddb8b45a9790809d6f492ba73acc9b383c573b
Devora <kister1010@gmail.com>
2019-09-02 16:57:40 +0300

    filters on the images

---
+++ ClientSide/src/app/models/image.ts
@@ -0,0 +1,17 @@
+export class Image {
+    id: number;
+    name: string;
+    url: string;
+    isBlur: boolean;
+    isClosedEye: boolean;
+    isDark: boolean;
+    isCutFace: boolean;
+    isGroom: boolean;
+    isLight: boolean;
+    isInside: boolean;
+    hasChildren: boolean;
+    hasYoung: boolean;
+    hasAdults: boolean;
+    numPerson: number;
+    isInRecycleBin: boolean;
+}
