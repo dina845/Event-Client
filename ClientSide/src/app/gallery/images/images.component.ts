f1214b252edbf3e9d1ce375bf27f9c55150c69b1
yehudit <cyehudit10@gmail.com>
2019-09-03 22:55:37 +0300

    fix the images

--- ClientSide/src/app/gallery/images/images.component.ts
+++ ClientSide/src/app/gallery/images/images.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef, Input } from '@angular/core';
 import { ImagesService } from 'src/app/services/images.service';
 import { Image } from 'src/app/models/image';
-import { saveAs } from "file-saver";
+// import { saveAs } from "file-saver";
 import * as JSZip from 'jszip';
 import { Url } from 'src/app/services/url';
 @Component({
@@ -15,7 +15,7 @@ export class ImagesComponent implements OnInit {
   public num = [1, 2, 3, 4, 5];
   selected: boolean = false;
   fileToUpload: File = null;
-  currentUrl: Url = new Url;
+  currentUrl: Url ;
   ngOnInit() {
 
   }
@@ -30,20 +30,17 @@ export class ImagesComponent implements OnInit {
         _formData.append("file", this.fileToUpload);
         var reader = new FileReader();
         reader.onload = (event: any) => {
+          this.currentUrl=new Url();
           this.currentUrl.urlImage = event.target.result;
           this.currentUrl.nameImage = this.fileToUpload.name;
-          this.urls.push(this.currentUrl);// to delete
+          this.urls.push(this.currentUrl);
           debugger;
           console.log(event.target.result);
         }
+        reader.readAsDataURL(files[i]);
+        console.log(JSON.stringify(_formData));
       }
-      for (let index = 0; index < files.length; index++) {
-        reader.readAsDataURL(files[index]);
-        console.log(JSON.stringify(_formData))
-
-      }
-
-
+     
 
       this.downZip(files, files.length);
       this.InsertImages(_formData);//send the images' url to the server = in order to init the table
@@ -76,7 +73,7 @@ export class ImagesComponent implements OnInit {
     zip.generateAsync({ type: "blob" })
       // saveAs(zip, "DevoraZip.zip");
       .then(function (blob) {
-        saveAs(blob, "photos.zip");
+        // saveAs(blob, "photos.zip");
 
       });
   }
