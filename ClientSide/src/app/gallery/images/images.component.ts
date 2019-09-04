64cf60b4cb2910385c658b3d36030cde7c466512
yehudit <cyehudit10@gmail.com>
2019-09-04 11:07:13 +0300

    4/9/19

--- ClientSide/src/app/gallery/images/images.component.ts
+++ ClientSide/src/app/gallery/images/images.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef, Input } from '@angular/core';
 import { ImagesService } from 'src/app/services/images.service';
 import { Image } from 'src/app/models/image';
-// import { saveAs } from "file-saver";
+import { saveAs } from "file-saver";
 import * as JSZip from 'jszip';
 import { Url } from 'src/app/services/url';
 @Component({
@@ -14,23 +14,25 @@ export class ImagesComponent implements OnInit {
   urls: Url[] = new Array;
   public num = [1, 2, 3, 4, 5];
   selected: boolean = false;
-  fileToUpload: File = null;
-  currentUrl: Url ;
+  fileToUpload: File;
+  currentUrl: Url;
+  Files: FileList;
   ngOnInit() {
 
   }
 
   handleFileInput(files: FileList) {
-
+    this.Files = files;
     if (files && files[0]) {
       let _formData = new FormData();
       for (let i = 0; i < files.length; i++) {
+        var fileToUpload: File = null;
         this.fileToUpload = files.item(i);
 
         _formData.append("file", this.fileToUpload);
         var reader = new FileReader();
         reader.onload = (event: any) => {
-          this.currentUrl=new Url();
+          this.currentUrl = new Url();
           this.currentUrl.urlImage = event.target.result;
           this.currentUrl.nameImage = this.fileToUpload.name;
           this.urls.push(this.currentUrl);
@@ -40,9 +42,9 @@ export class ImagesComponent implements OnInit {
         reader.readAsDataURL(files[i]);
         console.log(JSON.stringify(_formData));
       }
-     
 
-      this.downZip(files, files.length);
+
+      // this.downZip(files, files.length);
       this.InsertImages(_formData);//send the images' url to the server = in order to init the table
     }
     this.selected = true;
@@ -60,20 +62,20 @@ export class ImagesComponent implements OnInit {
     });
   }
 
-  downZip(files: FileList, size: number) {
+  downZip() {
     var zip = new JSZip();
     // zip.file("Hello.txt", "Hello World\n");
     var img = zip.folder("images");
-    for (let i = 0; i < size; i++) {
-      this.fileToUpload = files.item(i);
+    for (let i = 0; i < this.Files.length; i++) {
+      this.fileToUpload = this.Files.item(i);
 
       img.file(this.fileToUpload.name, this.fileToUpload, { File: true });
     }
     // img.file("smile.jpg ", "https://upload.wikimedia.org/wikipedia/commons/9/9f/Una-presidents-home.jpg");
     zip.generateAsync({ type: "blob" })
-      // saveAs(zip, "DevoraZip.zip");
+      // saveAs(zip, "PhotoZip.zip")
       .then(function (blob) {
-        // saveAs(blob, "photos.zip");
+        saveAs(blob, "photos.zip");
 
       });
   }
