e5c9d2400c4192397c771f05b219d33c0b743ec4
yehudit <cyehudit10@gmail.com>
2019-09-14 22:07:14 +0300

    changed icon

--- ClientSide/src/app/home/home.component.ts
+++ ClientSide/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ImagesService } from '../services/images.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,10 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private imageService:ImagesService) { }
 
   ngOnInit() {
+    this.imageService.isHome=true;
   }
   goToImage(){
 this.router.navigate(['gallery']);
