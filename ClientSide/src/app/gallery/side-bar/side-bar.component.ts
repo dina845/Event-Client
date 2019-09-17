23b1ebe2e0ff441dd3ec5132362df8bd63828436
Devora <kister1010@gmail.com>
2019-09-17 11:52:23 +0300

    clear and more

--- ClientSide/src/app/gallery/side-bar/side-bar.component.ts
+++ ClientSide/src/app/gallery/side-bar/side-bar.component.ts
@@ -38,7 +38,42 @@ export class SideBarComponent implements OnInit {
   }
   showCycle() {
     this.imagesService.showCycle = !this.imagesService.showCycle;
+this.gotoBotton();
   }
+  isShow: boolean = false;
+  topPosToStartShowing = 100;
+  @HostListener('window:scroll')
+  checkScroll() {
+
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+  gotoBotton() {
+    if(this.imagesService.showCycle)
+    window.scroll({
+      top: 1000000000000,
+      left: 0,
+      behavior: 'smooth'
+    });
+    if(!this.imagesService.showCycle)
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+    // break;
+  }
+
   filterAflerFalse() {
     this.imagesService.imageTemp = this.imagesService.imageMain;
     if (this.filterImage.isBlur == true)
