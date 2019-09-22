c06cd7d951ae74a8ef431e941914fd723a6d2f20
yehudit <cyehudit10@gmail.com>
2019-09-22 21:33:16 +0300

    client

--- ClientSide/src/environments/environment.prod.ts
+++ ClientSide/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  baseRoute:"http://34.90.8.20/WebApi/",
+  // baseRoute:"http://34.90.8.20/WebApi/",
+  baseRoute: "http://35.239.144.95/WebApi/",
   production: true,
   domain : "/takeapeek/",
   assets:"/"
